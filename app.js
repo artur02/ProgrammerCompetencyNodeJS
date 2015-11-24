082484cc5005cae82b4d2685abd4cbac6e840c31
Artur Herczeg <Artur_Herczeg@epam.com>
2015-11-24 14:20:45 +0100

    fix for iisnode

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var http = require('http');
 
+var process = process || { env : { PORT: 8080 }}
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+}).listen(process.env.PORT);
 
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:' + process.env.PORT + '/');
