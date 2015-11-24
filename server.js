ad79441f47e36f9c83e78d3d1a101dc2b86c6ee5
Artur Herczeg <Artur_Herczeg@epam.com>
2015-11-24 15:48:45 +0100

    code cleanup

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 "use strict";
 
-// var http = require('http')
-// var port = process.env.PORT || 1337;
-// http.createServer(function(req, res) {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.end('Hello World\n');
-// }).listen(port);
+const ProgCompServer = require('./ProgComp/progcomp')
 
-var ProgCompServer = require('./ProgComp/progcomp')
+const koa = require('koa');
+const app = koa();
+const port = process.env.PORT || 1337;
 
-var koa = require('koa');
-var app = koa();
-var port = process.env.PORT || 1337;
-console.log("Listening on port: " + port);
 
-var progComp = new ProgCompServer(app);
+const progComp = new ProgCompServer(app);
 progComp.init();
 
-// app.use(function *(){
-//   this.body = 'Hello World';
-// });
-
 app.listen(port);
+console.log("Listening on port: " + port);
