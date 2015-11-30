8e770e92ee7ca03ddfcd774f8766a029bc9069d9
Artur Herczeg <Artur_Herczeg@epam.com>
2015-11-30 13:36:20 +0100

    config, logging, azure blob

--- ProgComp/progcomp.js
+++ ProgComp/progcomp.js
@@ -1,19 +1,24 @@
 "use strict";
 
+const BlobStorage = require('./blob');
+
 class ProgCompServer
 {	
 	static get ROOTDIR(){
 		return "/ProgComp/";
 	}
 	
-	constructor(app, render) {
-		this.app = app;
-		this.render = render;
-		
+	constructor(web, azure) {
+		this.logger = web.logger;
+		this.app = web.app;
+		this.render = web.render;
+		this.storage = new BlobStorage(azure, web);
 	}
 	
-	init(){
-		this.app.use(function *(){
+	init() {
+		this.logger.info('Starting ProgComp module...');
+		
+		this.app.use(function *() {
 			yield this.render('ProgComp/Views/index.jade', {name: "World"});
 		});
 	}
--- server.js
+++ server.js
@@ -2,12 +2,31 @@
 
 const ProgCompServer = require('./ProgComp/progcomp')
 
+const nconf = require('nconf');
 const views = require('koa-views');
 const koa = require('koa');
 const app = koa();
+const storage = require('azure-storage');
+const winston = require('winston');
 
-const port = process.env.PORT || 1337;
+// Configuration setup
+nconf
+    .env()
+    .file({ file: 'config.json', search: true })
+    .defaults({
+      'PORT': 1337
+    });
 
+const logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)(),
+      new (winston.transports.File)({ filename: 'serverlog.log' })
+    ]
+  });
+  
+logger.info('Application starting...');
+
+// Rendering engine setup
 var render = views('', {
   map: {
     html: 'jade',
@@ -16,14 +35,22 @@ var render = views('', {
 });
 app.use(render);
 
+logger.info('Renderer configured');
 
-
-// app.use(function* (next) {
-//   yield this.render('ProgComp/Views/index.jade');
-// });
-
-const progComp = new ProgCompServer(app, render);
+// Programmer competency application
+const progComp = new ProgCompServer(
+  { app: app, 
+    render: render,
+    config: nconf,
+    logger: logger
+  }, 
+  {
+    storage: storage,
+  });
 progComp.init();
 
+// Start server
+logger.info('Server starting...');
+const port = nconf.get('PORT');
 app.listen(port);
-console.log("Listening on port: %d", port);
+logger.info("Listening on port %d", port);
